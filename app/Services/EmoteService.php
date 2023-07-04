3e5c2e9c04a8ca1c6a247abba340c1c9b7102135
Tobiasz Tonn <tonn@delta.poznan.pl>
2023-07-04 12:32:49 +0200

    Dodanie kodu do metod

--- app/Services/EmoteService.php
+++ app/Services/EmoteService.php
@@ -3,18 +3,67 @@
 namespace App\Services;
 
 use App\Models\Emote;
+use App\Models\User;
 
 class EmoteService
 {
     public function getUserEmotesAndIcons($data){
+        $user = User::where('token', $data->token)->get();
 
+        if(!$user){
+            $response = [
+                'message' => "Brak użytkownika"
+            ];
+            return response($response, 404);           
+        }
+
+        $icons = Emote::where('user_id', $user->id)->get()
+                    ->where('type', 'icon');
+        $emotes = Emote::where('user_id', $user->id)->get()
+                    ->where('type', 'emote');
+
+        $response = [
+            'icons' => $icons,
+            'emotes' => $emotes
+        ];
+        return response($response, 201);
     }
 
     public function getUserEmotes($data){
-        
+        $user = User::where('token', $data->token)->get();
+
+        if(!$user){
+            $response = [
+                'message' => "Brak użytkownika"
+            ];
+            return response($response, 404);           
+        }
+
+        $emotes = Emote::where('user_id', $user->id)->get()
+                    ->where('type', 'emote');
+
+        $response = [
+            'emotes' => $emotes
+        ];
+        return response($response, 201);
     }
 
     public function getUserIcons($data){
-        
+        $user = User::where('token', $data->token)->get();
+
+        if(!$user){
+            $response = [
+                'message' => "Brak użytkownika"
+            ];
+            return response($response, 404);           
+        }
+
+        $icons = Emote::where('user_id', $user->id)->get()
+                    ->where('type', 'icon');
+
+        $response = [
+            'icons' => $icons
+        ];
+        return response($response, 201);
     }
 }
