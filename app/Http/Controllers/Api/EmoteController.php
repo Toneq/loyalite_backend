1184568775a9d63e7b8986cb96e7d0a19db33074
Tobiasz Tonn <tonn@delta.poznan.pl>
2023-07-04 12:40:49 +0200

    Dodanie kodu w kontrolerze emotek

--- app/Http/Controllers/Api/EmoteController.php
+++ app/Http/Controllers/Api/EmoteController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use App\Services\EmoteService;
 
 class EmoteController extends Controller
@@ -13,5 +12,17 @@ class EmoteController extends Controller
     public function __construct(EmoteService $emoteService)
     {
         $this->emoteService = $emoteService;
+    }
+
+    public function get_user_emotes_and_icons($data){
+        return $this->emoteService->getUserEmotesAndIcons($data);
+    }
+
+    public function get_user_emotes($data){
+        return $this->emoteService->getUserEmotes($data);
+    }
+
+    public function get_user_icons($data){
+        return $this->emoteService->getUserIcons($data);
     }
 }
